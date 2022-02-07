d1f9e1aa624aded788d5c6bd7577f15d57f7e43d
GeorgeM101 <georgmboya@gmail.com>
2022-02-07 18:15:06 +0300

    added sign in form

--- app.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask,render_template , app
-
-
-@app.route("/")
-def index():
-    return render_template('nav-bar.html')
-app = Flask(__name__)
--- app/models.py
+++ app/models.py
@@ -1,32 +1,7 @@
-from . import db
-from . import login_manager
-from flask_login import UserMixin
-from werkzeug.security import generate_password_hash,check_password_hash
+from ensurepip import bootstrap
+from flask_wtf import FlaskForm
+from wtforms.validators import InputRequired,Email,Length
+from wtforms import StringField, PasswordField, BooleanField
 
-class User(db.Model):
-    pass_secure = db.Column(db.string(200))
-
-    @property
-    def password(self):
-        raise AttributeError('You cannot read the password attribute')
-
-    @password.setter
-    def password(self, password):
-        self.pass_secure = generate_password_hash(password)
-
-    def verify_password(self,password):
-        return check_password_hash(self.pass_secure,password)
-
-class User(UserMixin,db.Model):
-    __tablename__ = 'users'
-
-    id = db.Column(db.Integer,primary_key = True)
-    username = db.Column(db.String(255),index = True)
-    email = db.Column(db.String(255),unique = True,index = True)
-    role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
-    password_hash = db.Column(db.String(255))
-
-
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
+app=Flask(__name__)
+bootstrap(app)
