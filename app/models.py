348ff7f25931107e9ff4bfda40eaeb6b543c3831
GeorgeM101 <georgmboya@gmail.com>
2022-02-08 02:33:03 +0300

    added sqlachemy

--- app/models.py
+++ app/models.py
@@ -3,5 +3,7 @@ from flask_wtf import FlaskForm
 from wtforms.validators import InputRequired,Email,Length
 from wtforms import StringField, PasswordField, BooleanField
 
-app=Flask(__name__)
-bootstrap(app)
+class loginForm(FlaskForm):
+    username = StringField('Username', validators=[InputRequired(), Length(min=5, max=20)])
+    password = StringField('password', validators=[InputRequired(), Length(min=8, max=50)])
+    remember = BooleanField('remember me')
--- run.py
+++ run.py
@@ -1,11 +1,15 @@
 from flask import Flask, render_template
 
+from app.models import loginForm
+
 app= Flask(__name__)
 
-@app.route('/index')
-def index():
-    return render_template('index.html')
 
+@app.route('/login')
+def login():
+    form = loginForm
+
+    return render_template('login.html', form=form)
 
 if __name__== '__main__':
     app.run(debug=True)
